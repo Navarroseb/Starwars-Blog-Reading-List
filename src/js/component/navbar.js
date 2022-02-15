48478b6fc3f126c0d42eaafa03a02dd1f735d383
Navarroseb <sebastnavarro@gmail.com>
2022-02-15 21:46:32 +0000

    subiendo mp3

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import starwarslogo from "../../img/Starwars_logo.png";
 import darthvader from "../../img/Darth_Vader.png";
+/* import song from "../../img/Mp3.mp3" */
+
 
 export const Navbar = () => {
 	return (
@@ -11,13 +13,18 @@ export const Navbar = () => {
 					<img src={starwarslogo} alt="" width="60" height="60" />
 					<img src={darthvader} alt="" width="60" height="60" />
 				</a>
-				<div className="dropdown">			
-				<a class="nav-link dropdown-toggle bg-primary text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">
-					Favorites
-				</a>
-				<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-					<li><a class="dropdown-item" href="#">Favorites</a></li>
-				</ul>
+				<audio id="audio2"
+					preload="auto"
+					src="your_mp3_file_path.mp3"
+					oncanplaythrough="this.play();">
+				</audio>
+				<div className="dropdown">
+					<a class="nav-link dropdown-toggle bg-primary text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+						Favorites
+					</a>
+					<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<li><a class="dropdown-item" href="#">Favorites</a></li>
+					</ul>
 				</div>
 			</div>
 		</nav>
