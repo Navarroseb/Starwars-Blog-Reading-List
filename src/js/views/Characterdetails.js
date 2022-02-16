57bc4712253078d57ee3d39588fe79c3568daeb7
Navarroseb <sebastnavarro@gmail.com>
2022-02-16 16:11:23 +0000

    actualizando

--- src/js/views/Characterdetails.js
+++ src/js/views/Characterdetails.js
@@ -5,23 +5,20 @@ const Charactersdetails = () => {
     return (
 
 
-        <div classNameName="container d-flex justify-content-center" style={{width:"auto"}}>
+        <div classNameName="container d-flex justify-content-center" >
             <div>
                 <img style={{width:"85%"}} src="https://www.lavanguardia.com/files/article_main_microformat/uploads/2020/05/04/5fa922920d3b5.png"
                 />
                 
-                <div className="card mb-3" style={{ maxwidth:"540px"}} style={{ marginTop: "15px"}}>
+                <div className="card mx-5" style={{ marginTop: "15px"}}>
                     <div className="row g-0">
                         <div className="col-md-4">
-                            <img src="https://c4.staticflickr.com/8/7481/27348202211_641e6dafe6.jpg" className="img-fluid rounded-start" />
+                            <img src="https://www.independent.ie/regionals/kerryman/news/b94fb/34250758.ece/AUTOCROP/w620/2015-12-02_ker_15039481_I1.JPG" className="img-fluid rounded-start" />
                         </div>
                         <div className="col-md-8">
                             <div className="card-body">
                                 <h5 className="card-title">Nombre del personaje</h5>
                                 <ul className="list-group">
-                                    <li className="list-group-item">
-                                        Características del personaje
-                                    </li>
                                     <li className="list-group-item">
                                         Características del personaje
                                     </li>
