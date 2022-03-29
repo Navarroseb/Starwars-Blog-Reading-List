81d2b775c9a5d66696c5bbfbec65d04d1fb7bbae
Navarroseb <sebastnavarro@gmail.com>
2022-03-29 23:16:56 +0000

    navbar listo, falta favoritos

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -7,28 +7,31 @@ import starwarslogo from "../../img/starwarslogo.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<a className="navbar-brand" href="#">
+		<nav className="navbar navbar-expand-lg bg-light">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="/">
 					<img src={starwarslogo} alt="" width="90" height="70" />
 				</a>
-					<ul>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav m-auto">
 						<li className="nav-item">
-							<Link to="/" className="nav-link active" aria-current="page">
+							<Link to="/" className="nav-link active text-secondary" >
 								<span className="navbar-brand mb-0 h1">Home</span>
 							</Link>
 						</li>
 						<li className="nav-item">
-							<Link to="/Characters" className="nav-link">
+							<Link to="/Characters" className="nav-link text-secondary">
 								<span className="navbar-brand mb-0 h1">Characters</span>
 							</Link>
 						</li>
 						<li className="nav-item">
-							<Link to="/planets" className="nav-link">
+							<Link to="/planets" className="nav-link text-secondary">
 								<span className="navbar-brand mb-0 h1">Planets</span>
 							</Link>
 						</li>
 					</ul>
+					</div>
+
 					{/* <Dropdown className="ms-3">
 						<Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
 							Favoritos <Badge bg="danger">{store.favoritos.length}</Badge>
@@ -47,7 +50,7 @@ export const Navbar = () => {
 					<a className="nav-link dropdown-toggle bg-primary text-white bg-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">
 						Favorites
 					</a>
-					<ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+					<ul className="dropdown-menu text-secondary" aria-labelledby="navbarDropdown">
 						<li><a className="dropdown-item" href="#">Favorites</a></li>
 					</ul>
 				</div>
