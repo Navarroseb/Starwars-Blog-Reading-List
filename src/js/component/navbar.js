f3cd66312f64d87dd627cb96d0c8c7761c246a57
Navarroseb <sebastnavarro@gmail.com>
2022-04-08 20:19:09 +0000

    revisando componentes

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Badge, Dropdown } from "react-bootstrap";
 import starwarslogo from "../../img/starwarslogo.png";
 
 export const Navbar = () => {
--- src/js/layout.js
+++ src/js/layout.js
@@ -12,17 +12,14 @@ import { Planetsdetails} from "./views/Planetsdetails";
 import injectContext from "./store/appContext";
 
 
-//create your first component
+
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
 
 	return (
-		<div>
+		<>
 			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
+				<Navbar />
 					<Switch>
 						<Route exact path="/">
 							<Home />
@@ -43,9 +40,8 @@ const Layout = () => {
 						</Route>
 					</Switch>
 					<Footer />
-				</ScrollToTop>
 			</BrowserRouter>
-		</div>
+		</>
 	);
 };
 
